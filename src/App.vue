06338fbd2435caa3f3865fe5513d2675e045670f
rancerun <lancesun22@gmail.com>
2020-01-08 23:30:40 -0500

    initialize new app design

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,6 @@
 <template>
   <div id="app">
-    <hr class="vert-line">
     <div>
-      <div class="header-text">
-        <span class="title-bar">
-          <div class="name-text">Lance</div>
-          <input
-            placeholder="Search"
-            @blur="setToEmpty"
-          >
-        </span>
-        <div class="sub-text">
-          Programmer
-        </div>
-      </div>
-      <hr class="hori-line">
       <div class="columns">
         <column
           v-for="(topic, index) in kanban"
@@ -64,39 +50,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   margin-bottom: 16px;
-}
-
-.vert-line {
-  size: all;
-  width: 1;
-  margin-left: 32px;
-}
-
-.hori-line {
-  width: 100%;
-  margin-top: 32px;
-  margin-bottom: 32px;
-}
-
-.header-text {
-  margin-top: 32px;
-  margin-left: 64px;
-}
-
-.title-bar {
-  display: flex;
-  flex-direction: row;
-}
-
-.name-text {
-  font-size: 32px;
-  font-weight: 800;
-  margin-bottom: 8px;
-}
-
-.sub-text {
-  color: #828491;
-  font-size: 16px;
+  background-color: #29BE80;
 }
 
 .columns {
@@ -108,27 +62,5 @@ export default {
   margin-right: 32px;
 }
 
-hr {
-  opacity: 0.2;
-}
-
-input {
-  height: 28px;
-  width: 200px;
-  font-size: 16px;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  border-color: #f0f2f5;
-  border-radius: 48px;
-  border-style: solid;
-  border-width: 1px;
-  margin-left: auto;
-  margin-right: 32px;
-  padding: 10px;
-  outline: none;
-}
-
-input:focus {
-  outline: none;
-}
 
 </style>
